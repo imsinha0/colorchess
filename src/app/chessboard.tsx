54f6d3f2abd206bb7582716b0eac5e5e16e9741b
ishaan <ishaan.mohan.sinha@gmail.com>
2024-10-20 23:14:03 -0400

    Board centered

---
+++ src/app/chessboard.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { useState } from "react"
+
+class Board{
+    fields = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+}
+
+export function Chessboard(){
+    const [currentBoard, setCurrentBoard] = useState(new Board())
+    return (
+        <div className="shadow-lg w-800 h-800 bg-yellow-300">
+            
+        </div>
+    )
+}
